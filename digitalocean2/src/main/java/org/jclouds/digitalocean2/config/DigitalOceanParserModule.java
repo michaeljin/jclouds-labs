a23dfe90f4dd8e8dc3b0dcb63c8e482bc33aab9f
Michael Jin <michael@mesosphere.io>
2014-11-11 17:02:29 -0800

    Fix checkstyle.

--- digitalocean2/src/main/java/org/jclouds/digitalocean2/config/DigitalOceanParserModule.java
+++ digitalocean2/src/main/java/org/jclouds/digitalocean2/config/DigitalOceanParserModule.java
@@ -16,7 +16,6 @@
  */
 package org.jclouds.digitalocean2.config;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Throwables.propagate;
 import static com.google.common.collect.Iterables.get;
